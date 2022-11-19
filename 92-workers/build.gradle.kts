1190b627ac8d12f425364cfa1ed4174929070362
pianinator <113355583+pianinator@users.noreply.github.com>
2022-11-19 11:26:07 +0000

    Deploying to exercises from @ APICe-at-DISI/exercises@5f14ee3884361781411e6476c3cbfea56b6c22f2 🚀

--- 92-workers/build.gradle.kts
+++ 92-workers/build.gradle.kts
@@ -11,10 +11,10 @@ repositories {
 
 dependencies {
     // Use JUnit Jupiter API for testing.
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.4.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
 
     // Use JUnit Jupiter Engine for testing.
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.4.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.1")
 }
 
 val mainClass: String by project
