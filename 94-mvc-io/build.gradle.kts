c63ed1587d64a00b85f469db5d27f18bba92a5e1
DanySK <DanySK@users.noreply.github.com>
2024-06-18 16:39:30 +0000

    Deploying to solutions from @ APICe-at-DISI/exercises@06205a1da72f16590f3a396dc091e6cc12ff59b9 🚀

--- 94-mvc-io/build.gradle.kts
+++ 94-mvc-io/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("com.github.spotbugs:spotbugs-annotations:4.8.6")
+    compileOnly("com.github.spotbugs:spotbugs-annotations:4.8.5")
 }
 
 application {
